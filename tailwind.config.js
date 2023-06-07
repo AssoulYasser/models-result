f0bb8874a336ad811b4b03155773981bc98ad576
AssoulYasser <assoulyasser01@gmail.com>
2023-06-07 02:59:45 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary:'#0445FF',
+                secondary:'#E8E8E8',
+            }
+        },
+    },
+    plugins: [],
+}
+
